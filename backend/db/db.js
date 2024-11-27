a3e0ea48cdf47afdd54ff208546663e8921a50f2
salaudeen22 <salaudeensalu@gmail.com>
2024-11-27 11:50:38 +0530

    done connecting to backend

---
+++ backend/db/db.js
@@ -0,0 +1,18 @@
+const mongoose = require('mongoose');
+
+const connectDB = async () => {
+  try {
+    const conn = await mongoose.connect(process.env.MONGODB_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    console.log(`MongoDB Connected: ${conn.connection.host}`);
+  } catch (error) {
+    console.error(`Error connecting to MongoDB: ${error.message}`);
+   
+    console.log('Retrying connection in 5 seconds...');
+    setTimeout(connectDB, 5000);
+  }
+};
+
+module.exports = connectDB;
--- backend/index.js
+++ backend/index.js
@@ -1,13 +1,16 @@
 const express = require("express");
 require("dotenv").config();
-
+const connectDB = require("./db/db");
 const app = express();
+const cors = require("cors");
 
+
+app.use(cors());
 app.use(express.json());
 
-app.use("/", (req, res) => {
-  res.send(`App is listening at ${PORT}`);
-});
+connectDB();
+
+
 
 const PORT = process.env.PORT || 3999;
 app.listen(PORT, () => {
